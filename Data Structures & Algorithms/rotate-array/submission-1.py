c15a90b26e7893ae3abc0abf1ae6865b2c12c75d
Xtramous <nitinkamal132@gmail.com>
2026-08-10 15:57:17 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        def rev(l,r):
+            while l<r:
+                nums[l],nums[r] = nums[r],nums[l]
+                l +=1
+                r -=1
+        n = len(nums)
+        k %= n
+        rev(0,n-1)
+        rev(0,k-1)
+        rev(k,n-1)
+
+        return nums
+
+        
